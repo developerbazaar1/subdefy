07a5a43141c13fa96e909c18712021c1781ca9bd
Alok soni <db.aloksoni@gmail.com>
2023-07-06 23:02:21 -0700

    add favicon

--- dashboard.php
+++ dashboard.php
@@ -11,6 +11,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <!-- Main CSS-->
    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!--favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/subdefy-favicon.png">
    <!-- Font-icon css-->
    <link rel="stylesheet" type="text/css"
     href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
