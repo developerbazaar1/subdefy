e7f4170cf2e80e1f8f11c19ba4f2fa97ed1f52d4
Alok soni <db.aloksoni@gmail.com>
2023-07-09 23:28:01 -0700

    working on setting and fontstyle

--- setting.php
+++ setting.php
@@ -59,7 +59,6 @@
 		<script src="assets/js/popper.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
 		<script src="assets/js/main.js"></script>
-		<!-- The javascript plugin to display page loading on top-->
 		<script src="assets/js/plugins/pace.min.js"></script>
 	</body>
 </html>
